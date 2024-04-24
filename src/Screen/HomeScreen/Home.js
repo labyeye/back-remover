eaee8cbb09a4a841266310bafb04031206a3a493
Labh <labh@Labhs-MacBook-Pro.local>
2024-04-25 04:40:52 +0530

    Made Home Screen

---
+++ src/Screen/HomeScreen/Home.js
@@ -0,0 +1,44 @@
+import LottieView from "lottie-react-native";
+import React from "react";
+import { StyleSheet ,Text,Touchable,TouchableOpacity,View } from "react-native";
+
+const Home = () => {
+    return(
+        <View style={styles.container}>
+            <LottieView style ={{marginTop:100,justifyContent:'flex-end',width:"100%",height:"40%"}}source={require('../../assets/Animations/image.json')} loop autoPlay />
+            <Text style={styles.name}>Back Remove Pro</Text>
+            <Text style={styles.name1}>Helped by remove.bg</Text>
+            <TouchableOpacity style={styles.button}>
+                <Text>Get Started</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'black',
+        alignItems:'center'
+    },
+    name:{
+        color:'white',
+        textAlign:'center',
+        fontSize:40
+    },
+    name1:{
+        color:'white',
+        textAlign:'center',
+        fontSize:20
+    },
+    button:{
+        width:"80%",
+        height:"6%",
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'white',
+        marginTop:40
+    }
+})
+
+export default Home;
