030d5824c588149dd1fb45f657eca26c259acdca
Labh <labhbothra1380@gmail.com>
2024-07-18 22:32:00 +0530

    All Done Ready For Publish

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -9,17 +9,17 @@ import {
   TouchableOpacity,
   Dimensions,
   ScrollView,
+  ImageBackground,
 } from 'react-native';
-import { launchImageLibrary } from 'react-native-image-picker';
+import {launchImageLibrary} from 'react-native-image-picker';
 import axios from 'axios';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 const App = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [processedImage, setProcessedImage] = useState(null);
-  
 
   const selectImage = () => {
     launchImageLibrary({}, response => {
@@ -74,27 +74,32 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <TouchableOpacity style={styles.select} onPress={selectImage}>
-          <Text style={styles.buttonText}>Select Image</Text>
-        </TouchableOpacity>
-        {selectedImage && (
-          <Image source={{ uri: selectedImage.uri }} style={styles.image} />
-        )}
-        <TouchableOpacity style={styles.select} onPress={removeBackground}>
-          <Text style={styles.buttonText}>Remove Background</Text>
-        </TouchableOpacity>
-        {processedImage && (
-          <View style={styles.imageContainer}>
-            <Image source={{ uri: processedImage }} style={styles.image} />
-            <TouchableOpacity
-              style={styles.downloadButton}
-              onPress={openImageInBrowser}>
-              <Text style={styles.buttonText}>Download Image</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </ScrollView>
+      <ImageBackground
+        source={require('./src/assets/back.png')}
+        style={styles.background}
+        blurRadius={Platform.OS === 'ios' ? 8 : 3}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <TouchableOpacity style={styles.select} onPress={selectImage}>
+            <Text style={styles.buttonText}>Select Image</Text>
+          </TouchableOpacity>
+          {selectedImage && (
+            <Image source={{uri: selectedImage.uri}} style={styles.image} />
+          )}
+          <TouchableOpacity style={styles.select} onPress={removeBackground}>
+            <Text style={styles.buttonText}>Remove Background</Text>
+          </TouchableOpacity>
+          {processedImage && (
+            <View style={styles.imageContainer}>
+              <Image source={{uri: processedImage}} style={styles.image} />
+              <TouchableOpacity
+                style={styles.downloadButton}
+                onPress={openImageInBrowser}>
+                <Text style={styles.buttonText}>Download Image</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </ScrollView>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
@@ -114,12 +119,19 @@ const styles = StyleSheet.create({
     width: width * 0.8,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#0B666A',
+    backgroundColor: 'white',
     borderRadius: 25,
     marginVertical: 10,
+    shadowOffset: {width:0,height:5},
+    shadowOpacity: 100,
+    shadowColor: '#DEE0E5',
+    shadowRadius: 4,
+  },
+  background: {
+    flex: 1,
   },
   buttonText: {
-    color: '#97FEED',
+    color: '#093086',
     fontSize: 18,
   },
   imageContainer: {
@@ -130,15 +142,20 @@ const styles = StyleSheet.create({
     width: width * 0.8,
     height: width * 0.8,
     marginVertical: 20,
+    borderRadius:20,
   },
   downloadButton: {
     height: 50,
     width: width * 0.8,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#0B666A',
+    backgroundColor: 'white',
     borderRadius: 25,
     marginVertical: 10,
+    shadowOffset: {width:0,height:5},
+    shadowOpacity: 100,
+    shadowColor: '#DEE0E5',
+    shadowRadius: 4,
   },
 });
 
